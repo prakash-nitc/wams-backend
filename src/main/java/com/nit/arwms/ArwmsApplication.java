2854793468b32606d1fb8e5fd8db72676d083148
Prakash Kumar Sarangi <prakash_m251250cs@nitc.ac.in>
2026-01-31 19:17:39 +0530

    Initial Spring Boot project setup and successful startup

---
+++ src/main/java/com/nit/arwms/ArwmsApplication.java
@@ -0,0 +1,13 @@
+package com.nit.arwms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArwmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArwmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nit/arwms/ArwmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nit.arwms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArwmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
