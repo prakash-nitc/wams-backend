5a7537d1d32f399fdc3f2a630eb97a960bbd8b29
Prakash Kumar Sarangi <prakash_m251250cs@nitc.ac.in>
2026-02-06 18:01:18 +0530

    feat(workflow): add empty WorkflowService with @Service annotation

---
+++ src/main/java/com/nit/arwms/workflow/WorkflowService.java
@@ -0,0 +1,21 @@
+package com.nit.arwms.workflow;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Service layer for Workflow business logic.
+ * 
+ * This class handles all workflow-related business operations,
+ * keeping the controller focused on HTTP concerns only.
+ * 
+ * Key Concept: @Service Annotation
+ * --------------------------------
+ * - Marks this class as a Spring-managed service component
+ * - Spring automatically creates a single instance (singleton by default)
+ * - Can be injected into other components (like controllers)
+ * - Semantically indicates "this is where business logic lives"
+ */
+@Service
+public class WorkflowService {
+    // Business logic will be added in subsequent commits
+}
